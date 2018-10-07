632d7e13d398bbdfa686137b94ea7d9b4a6f2801
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-10-07 22:11:52 +0500

    fixed test

--- test/index.js
+++ test/index.js
@@ -1,4 +1,9 @@
 const path = require('path')
 const mathlib = require('../src')
 
-console.log(mathlib(path.resolve(__dirname, 'math.txt')))
+console.log(mathlib(
+`-6*-6-1-5/2/6-2+2/2/2
+2+2
+2/2
+2^8
+`))
